6a23583ae56cd11347789ca81f42239581b7868c
Ashley <ashleyhayre99@gmail.com>
2021-12-13 23:30:19 +0000

    CRUD functionality

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,6 +11,24 @@ class PostController extends Controller
 
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::orderBy('id', 'desc')->paginate(4);
+        return view('posts.index', ['posts' => $posts]);
+    }
+
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+
+        return view('posts.show', ['post' => $post]);
+    }
+
+    public function edit(Request $request, $id)
+    {
+        $post = Post::findOrFail($id);
+    }
+
+    public function destory($userID, $id)
+    {
+        $post = Post::findOrFail($id);
     }
 }
