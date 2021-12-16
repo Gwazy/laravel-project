36e8463ce7a2783df7ce536587bb18560615d9d9
Ashley <ashleyhayre99@gmail.com>
2021-12-16 21:04:18 +0000

    Redesigned comments page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,29 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1> Post : {{  $post -> title }}</h1>
-        <div>
-            {{ $post->post }}
-        </div>
+
+    <div class="container bg-light pt-3"> 
+        <h1 class="text-center mb-5"> Posts </h1>
+            <div class="container border rounded border-4 mt-3">
+                    <h1>{{ $post->title }}</h1>
+                    <small>Written by {{ $post->user->name }} on {{ $post->created_at }}</small>
+                    <p>{{  $post->post}} </p>
+            </div>
+
+            <div class="container">
+                <div class = "container align-self-center">
+                    @if (count($comments) != 0)
+                        @foreach($comments as $comment) 
+                            <div class="container border rounded border-4 mt-3">
+                                <p>{{ $comment->comment }}</p>
+                                <small>Written by {{ $comment->user->name }}</small>
+                                <span><small> on {{ $comment->created_at }}</small></span>
+                            </div>
+                        @endforeach
+                    @else
+                <div class = "text-center mt-3 pb-3">
+                    <h2>No comments found</h2>
+                </div>
+            </div>
+            @endif
+
     </div>
 
 
-    <div class="container">
-        <div class = "container align-self-center">
-            @if (count($comments) != 0)
-                @foreach($comments as $comment) 
-                    <div class='well'>
-                        <h2>{{ $comment->comment }}</h2>
-                        <small>Written by {{ $comment->user->name }}</small>
-                        <span><small> on {{ $comment->created_at }}</small></span>
-                    </div>
-                @endforeach
-            @else
-        <div class = 'center'>
-            <h2>No comments found</h2>
-        </div>
-    </div>
-    @endif
-    </div>
+
+
+
+
 @endsection
