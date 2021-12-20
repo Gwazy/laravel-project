72add016980b3e632d9cbbf4e5667d0f9252a80d
Ashley <ashleyhayre99@gmail.com>
2021-12-20 16:33:24 +0000

    Fixed service container

--- app/Services/Dog.php
+++ app/Services/Dog.php
@@ -16,8 +16,10 @@ class Dog
         $this->apiKey = $apiKey;
     }
 
+    //  https://docs.thedogapi.com/authentication
     public function getDogName()
     {
+        dd('https://api.thedogapi.com/v1/breeds?api_key=' . $this->apiKey);
         $response = Http::get('https://api.thedogapi.com/v1/breeds?api_key=' . $this->apiKey);
         return json_decode($response)[rand(0, 25)]->name;
     }
